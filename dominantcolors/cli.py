1c5897b60f04422a7ffb2695321c0bc8a2ff0631
pablonoya <pablonoya18@gmail.com>
2024-08-02 21:29:47 -0400

    Add CLI options for popup color, alpha blending and top bar color

--- dominantcolors/cli.py
+++ dominantcolors/cli.py
@@ -8,6 +8,19 @@ from .dominantcolors import color_extractor
 @click.option(
     "-c", "--target-contrast", default=3.0, show_default=True, help="Target contrast ratio"
 )
-def cli(image_path, target_contrast):
+@click.option("--bg-popup", default="#232A31", show_default=True, help="Background color of popup")
+@click.option(
+    "-a",
+    "--alpha",
+    default=208,
+    type=click.IntRange(0, 255),
+    show_default=True,
+    help="Alpha value for blending with background color",
+)
+@click.option(
+    "--bg-topbar", default="#303942", show_default=True, help="Background color of topbar"
+)
+def cli(image_path, target_contrast, bg_popup, alpha, bg_topbar):
     """Extract dominant colors from IMAGE_PATH"""
-    color_extractor(image_path, target_contrast)
+
+    color_extractor(image_path, target_contrast, bg_popup, alpha, bg_topbar)
--- dominantcolors/dominantcolors.py
+++ dominantcolors/dominantcolors.py
@@ -4,26 +4,28 @@ from math import isclose
 import click
 import numpy as np
 from fast_colorthief import get_palette
-from PIL import Image
-
-BG_WIDGET_COLOR = (35, 42, 49)
-BG_TOPBAR_COLOR = (48, 57, 66)
+from PIL import Image, ImageColor
 
 
 def to_hex(color):
     """Convert color to hex string"""
+
     r, g, b = color
     return f"#{r:02x}{g:02x}{b:02x}"
 
 
-def blend_colors(color, bg_color, alpha=0.5):
+def blend_colors(color, bg_color, alpha=255):
     """Blend color with background color"""
+
+    alpha /= 255
     bg_color = np.array(bg_color)
+
     return alpha * np.array(color) + (1 - alpha) * bg_color
 
 
 def luminance_value(value):
-    """get luminance value from color value"""
+    """Get luminance value from color value"""
+
     if value > 1:
         value /= 255
 
@@ -31,13 +33,15 @@ def luminance_value(value):
 
 
 def color_luminance(rgb_color):
-    """get luminance from rgb color"""
+    """Get luminance from rgb color"""
+
     R, G, B = [luminance_value(val) for val in rgb_color]
     return (0.2126 * R + 0.7152 * G + 0.0722 * B) + 0.05
 
 
 def contrast_ratio(color1, color2):
-    """get contrast ratio between color1 and color2"""
+    """Get contrast ratio between color1 and color2"""
+
     luminance1 = color_luminance(color1)
     luminance2 = color_luminance(color2)
 
@@ -48,6 +52,7 @@ def contrast_ratio(color1, color2):
 
 def adjust_closest_color(rgb_color, target_color, target_contrast=3):
     """Adjust rgb_color to have target_contrast contrast ratio with target_color"""
+
     r, g, b = np.array(rgb_color) / 255
     h, _, s = rgb_to_hls(r, g, b)
 
@@ -65,6 +70,7 @@ def adjust_closest_color(rgb_color, target_color, target_contrast=3):
 def get_highest_contrast_color(target_color, candidate_colors, target_contrast=3):
     """Get the first color from candidate_colors with contrast ratio >= target_contrast
     if no color is found, adjust the color with highest contrast_ratio"""
+
     contrast_ratios = []
     for color in candidate_colors:
         ratio = contrast_ratio(target_color, color)
@@ -79,6 +85,7 @@ def get_highest_contrast_color(target_color, candidate_colors, target_contrast=3
 
 def preprocess_image(image):
     """Preprocess image to exclude dark and saturated dominant colors"""
+
     width, height = image.size
     image = image.resize((width // 2, height // 2)).convert("HSV")
     image_hsv = np.array(image)
@@ -98,24 +105,10 @@ def preprocess_image(image):
     return image_arr
 
 
-def color_extractor(image_path, target_contrast):
-    """Obtain the dominant colors from an image and modify them to generate a palette
-    with a suitable contrast ratio for use in the popup and radial progressbar music widgets.
-
-    Receives a string of the image path and prints a list with 3 hex colors.
-    The first two colors are applied to the popup: one for the background
-    and the other for the UI components.
-    The third color is the accent color for the radial progress bar.
-
-    Args:
-        image_path (str): The path to the image.
-
-    Example:
-        >>> color_extractor("image_path")
-        #232A31
-        #61d6c6
-        #55b8e2
-    """
+def color_extractor(image_path, target_contrast, bg_popup, alpha, bg_topbar):
+    # convert color strings to rgb tuples
+    bg_popup = ImageColor.getrgb(bg_popup)
+    bg_topbar = ImageColor.getrgb(bg_topbar)
 
     image = Image.open(image_path)
     image_array = preprocess_image(image)
@@ -124,14 +117,14 @@ def color_extractor(image_path, target_contrast):
     most_dominant_color, *candidate_colors = get_palette(image_array, 3, quality=2)
     click.echo(to_hex(most_dominant_color))
 
-    # get ui components color
-    bg_darkened = blend_colors(most_dominant_color, BG_WIDGET_COLOR, alpha=208 / 255)
-    ui_components_color = get_highest_contrast_color(
-        bg_darkened, candidate_colors, target_contrast=target_contrast
+    # blend most dominant color with background
+    bg_blended = blend_colors(most_dominant_color, bg_popup, alpha)
+    fg_color = get_highest_contrast_color(
+        bg_blended, candidate_colors, target_contrast=target_contrast
     )
 
-    click.echo(to_hex(ui_components_color))
+    click.echo(to_hex(fg_color))
 
     # get mediabar progress border color
-    mediabar_progress_color = get_highest_contrast_color(BG_TOPBAR_COLOR, [most_dominant_color])
+    mediabar_progress_color = get_highest_contrast_color(bg_topbar, [most_dominant_color])
     click.echo(to_hex(mediabar_progress_color))
